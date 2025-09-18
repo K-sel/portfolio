ddbe1dd56b757d42264e67358fd425e37abd475c
Vince <jpinard@bluewin.ch>
2025-09-18 23:58:55 +0200

    adding auto dark/light clock

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,33 @@ const clock = computed(() => {
   return `${hours}:${minutes}:${sec}`;
 });
 
+const night = computed(() => {
+  const now = new Date(date.value);
+  const hours = now.getHours();
+
+  if (hours <= 6 || hours >= 22) {
+    document.querySelector("html").classList.add("dark");
+    return true;
+  }
+});
+
 setInterval(() => {
   date.value = Date.now();
 }, 1000);
 </script>
 
 <template>
-  <main class="m-3 md:m-6 lg:m-12 max-w-dvw flex flex-col gap-3 md:gap-6 lg:gap-12">
+  <main
+    class="m-3 md:m-6 lg:m-12 max-w-dvw flex flex-col gap-3 md:gap-6 lg:gap-12"
+  >
     <div class="box h-[70dvh] flex flex-col justify-between">
       <div class="flex flex-col gap-12">
-        <h1 class="text-2xl md:text-3xl lg:text-4xl">Jonathan Pinard 💻</h1>
+        <h1 class="text-2xl md:text-3xl lg:text-4xl">
+          Jonathan Pinard 
+          <span v-if="night">😴</span>
+          <span v-else>💻</span>
+        </h1>
+
         <div class="flex flex-col gap-6">
           <a class="link" href="#">Projets</a>
           <a class="link" href="#">A propos</a>
@@ -40,7 +57,22 @@ setInterval(() => {
           Je transforme mes idées en réalités, un projet à la fois.
         </h2>
         <div class="flex flex-row justify-between">
-          <p class="text-lg">UTC+1 ZH {{ clock }}</p>
+          <div class="flex flex-row gap-1.5 md:gap-6 items-center">
+            <div class="flex flex-row items-center gap-1.5">
+              <svg
+                width="16"
+                height="17"
+                viewBox="0 0 16 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="8" cy="8.5" r="8" fill="#FF0000" />
+              </svg>
+              <p v-if="night" class="hidden md:block text-lg">Closed for the night</p>
+              <p v-else class="hidden md:block text-lg">Open for work</p>
+            </div>
+            <p class="text-lg">UTC+1 ZH {{ clock }}</p>
+          </div>
           <div class="flex flex-row gap-3">
             <a class="link" href="https://github.com/K-sel">Github</a>
             <a class="link" href="https://www.linkedin.com/in/jonathanpnrd/"
